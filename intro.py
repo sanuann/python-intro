c5f2587d27f673bbf40b1335f48934457756a75a
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2023-06-19 15:35:16 +0000

    clean up code

--- intro.py
+++ intro.py
@@ -1,20 +1,3 @@
-
-# l1 = [1, 2, 3, 4, 5.23, 'sanu', 1, 3]
-# print(l1)
-
-# s1 = { 1, 2, 3 , 'test'}
-# print(s1)
-
-# print (type(s1))
-
-# a = None
-# b = False
-
-# print
-
-
-
-
 # take inputs from user
 a = int(input("Enter an integer number: "))
 b = int(input("Enter a second integer number: "))
